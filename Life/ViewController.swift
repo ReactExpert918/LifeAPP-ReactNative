c534139d95981c1648c8fa5edb059a1f474f7615
ReactExpert918 <67091975+liyun0221@users.noreply.github.com>
2020-06-24 10:28:22 +0800

    initial commit

---
+++ Life/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Life
+//
+//  Created by Yun Li on 2020/6/24.
+//  Copyright © 2020 Yun Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
