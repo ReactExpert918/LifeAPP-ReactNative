31ef75d4b880b7cb070ec3ea2438fc8fa55f394b
ReactExpert918 <67091975+liyun0221@users.noreply.github.com>
2020-06-27 04:24:37 +0800

    Revert "Merge branch 'develop' into feature/setting"
    This reverts commit 6b1ac04c069f671a7b2eb7ef2549da9244f7bf8d, reversing
    changes made to c6738d0bd306d30bea762f74a606bb005be3e1a3.

---
+++ Life/Controllers/SignIn/SignInViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SignInViewController.swift
+//  Life
+//
+//  Created by XianHuang on 6/23/20.
+//  Copyright © 2020 Yun Li. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onBackPressed(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+
+}
