3c79a95993d42d34ba0a5f4032bba8df05f39058
ReactExpert918 <flywing.tech.1214@hotmail.com>
2020-06-25 19:03:36 -0700

    added missing files

--- Life/AppDelegate.swift
+++ Life/AppDelegate.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        IQKeyboardManager.shared.enable = true
+        FirebaseApp.configure()
         return true
     }
 
---
+++ Life/Utils/Util.swift
@@ -0,0 +1,39 @@
+//
+//  Util.swift
+//  Life
+//
+//  Created by XianHuang on 6/24/20.
+//  Copyright © 2020 Yun Li. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Util{
+    /**
+    * Show alert dialog
+    *
+    * @param title Dialog title
+    * @param message Dialog message
+    * @return void
+    */
+    static func showAlert(vc: UIViewController, _ title: String, _ message: String){
+        let topWindow: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
+        topWindow?.rootViewController = UIViewController()
+        topWindow?.windowLevel = UIWindow.Level.alert + 1
+        
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: UIAlertController.Style.alert
+        )
+
+        alert.addAction(UIAlertAction(
+            title: "OK",
+            style: UIAlertAction.Style.default,
+            handler: nil
+        ))
+        vc.present(alert, animated: true, completion: nil)
+    }
+    
+}
